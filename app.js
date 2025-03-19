50bccf720884776651c239b44e51f41be34f27d4
FernandoSalinas36 <jeffx975@gmail.com>
2025-03-18 21:39:25 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+const ingresaAmigo = document.getElementById("amigo");
+const listaAmigos = [];
+const ListaAmigosF = document.getElementById("listaAmigos");
+const ResultadoF = document.getElementById("resultado");
+
+function agregarAmigo(){
+    
+    if(ingresaAmigo.value == ""){
+        alert("El campo de nombre esta vacio")
+    }else{
+    listaAmigos.push(ingresaAmigo.value);
+    ListaAmigosF.innerHTML += `<li>${ingresaAmigo.value}</li>`;
+    
+    }  
+};
+function sortearAmigo(){
+    const random = Math.floor(Math.random()*listaAmigos.length);
+    const amigoSecreto = listaAmigos[random];
+    ResultadoF.innerHTML = `<li>${amigoSecreto}</li>`;
+};
 
